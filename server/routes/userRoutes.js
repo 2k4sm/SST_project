842acd3420965715357f6dc44ac643eec5fd4e9f
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-10 11:41:49 +0530

    "adds register route and encrption of password using bcrypt"

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrpyt = require("bcrypt")
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    res.json("user created");
-});
+    try{
+        const userExists = await User.findOne({email : req.body.email})
+        if(userExists){
+            return res.send({
+                success : false,
+                message : "User already exists"
+            })
+        }
 
-router.post("/login", async (req, res) => {
+        const salt = await bcrpyt.genSalt(10)
+        const hashPassword = await bcrpyt.hash(req.body.password, salt)
+        req.body.password = hashPassword;
+
+        const newUser = new User(req.body);
+        await newUser.save()
+        
+        res.status(201).json('User created Succesfully');
+
+    }
+    catch(err){
+        res.json(err)
+    }
 
 });
 
