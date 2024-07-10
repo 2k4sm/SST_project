0794e9d3276974615e7beb9de74865da0e008ee0
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-10 11:44:28 +0530

    adds login route.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,12 +5,12 @@ const bcrpyt = require("bcrypt")
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try{
-        const userExists = await User.findOne({email : req.body.email})
-        if(userExists){
+    try {
+        const userExists = await User.findOne({ email: req.body.email })
+        if (userExists) {
             return res.send({
-                success : false,
-                message : "User already exists"
+                success: false,
+                message: "User already exists"
             })
         }
 
@@ -20,13 +20,36 @@ router.post("/register", async (req, res) => {
 
         const newUser = new User(req.body);
         await newUser.save()
-        
+
         res.status(201).json('User created Succesfully');
 
     }
-    catch(err){
+    catch (err) {
         res.json(err)
     }
+
+});
+
+
+router.post("/login", async (req, res) => {
+
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+        res.send({
+            success: false,
+            message: "User not found, Please Register"
+        })
+    }
+    const validPassword = await bcrpyt.compare(req.body.password, user.password)
+    if (!validPassword) {
+        return res.send({
+            success: false,
+            message: "Invalid Password"
+        })
+    }
+
+
+    res.status(201).json('User logged in Succesfully');
 
 });
 
